ac5de8822b79c99dd6d6316a4161f4b2075e10a4
Luís Dionísio Fernandes <luiscdf@turintel.com>
2015-02-08 04:40:00 +0000

    Create bodymassindex.py

---
+++ bodymassindex.py
@@ -0,0 +1,8 @@
+
+name = raw_input("What is your name?")
+gender = raw_input("what is your gender? please use(m)(f)")
+age = raw_input("Wow old are you "print name "?")
+
+
+print "your name is " + name "and you are" + age "years Old"
+
