bd273c0821262021e25b0d9bc71ab63da3629743
Luís Dionísio Fernandes <luiscdf@turintel.com>
2015-02-10 05:51:53 +0000

    Create oddoreven.py

---
+++ oddoreven.py
@@ -0,0 +1,14 @@
+print " "
+print "Hello there! I am a very smart program that will tell you if a number is even or odd"
+print " "
+x = int(raw_input("Please insert a number: "))
+print " "
+t = "The number " + str(x) + " is "
+if x%2 == 0:
+	print t + "even" + "!"
+else:
+	print t + "odd" + "!"
+print " "
+print " My master is learning how to program with Python! I am glad to help him!" 
+print " "
+print " "
